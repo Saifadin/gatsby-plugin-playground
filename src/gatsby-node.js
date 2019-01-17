8a71be530c576bfeb5c5d68410b2a740a171e123
Osamah Aldoaiss <osamah@aldoaiss.de>
2019-01-18 00:28:56 +0100

    add console.log

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -2,4 +2,6 @@ const expressPlayground = require('graphql-playground-middleware-express').defau
 
 exports.onCreateDevServer = ({ app }) => {
   app.get('/___playground', expressPlayground({ endpoint: '/___graphql' }));
+
+  console.log('Your Gatsby GraphQL Playground is running on http://[YOUR_LOCALHOST]/___playground');
 };
